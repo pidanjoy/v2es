2b62938c757b42639a6aff259e5047e2486820f9
pidanjoy <119912895+pidanjoy@users.noreply.github.com>
2023-09-03 21:31:07 +0800

    login page

--- lib/widget/my_app_bar_widget.dart
+++ lib/widget/my_app_bar_widget.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 
 class MyAppBar extends StatefulWidget implements PreferredSizeWidget {
-  const MyAppBar({super.key, required this.size, this.actions});
+  const MyAppBar(
+      {super.key,
+      required this.size,
+      this.actions,
+      this.leading,
+      this.title,
+      this.backIcon});
 
   final double size;
 
+  final IconData? backIcon;
+  final Widget? leading;
+  final Widget? title;
   final List<Widget>? actions;
 
   @override
@@ -20,18 +29,20 @@ class _MyAppBarState extends State<MyAppBar> {
     return AppBar(
       backgroundColor: const Color.fromRGBO(35, 29, 40, 1.0),
       automaticallyImplyLeading: false,
-      leading: IconButton(
-        icon: const Icon(
-          Icons.arrow_back_ios_new_rounded,
-          size: 18,
-        ),
-        color: Colors.white,
-        onPressed: () => Navigator.pop(context),
-      ),
-      title: const Text(
-        'V2EX',
-        style: TextStyle(color: Colors.white, fontSize: 16),
-      ),
+      leading: widget.leading ??
+          IconButton(
+            icon: Icon(
+              widget.backIcon ?? Icons.arrow_back_ios_new_rounded,
+              size: 18,
+            ),
+            color: Colors.white,
+            onPressed: () => Navigator.pop(context),
+          ),
+      title: widget.title ??
+          const Text(
+            'V2EX',
+            style: TextStyle(color: Colors.white, fontSize: 16),
+          ),
       actions: widget.actions ?? [],
     );
   }
